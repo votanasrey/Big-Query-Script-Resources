8d9d21f3899f008dbccaf39f3f9d441a7e77e884
Votana Srey <votanasrey@Votanas-MacBook-Pro.local>
2022-05-05 10:05:02 +0700

    commited

---
+++ BQDataAnalytics/OwnResources/BillingWithOrdersInformationScropt_Finance_Haing.sql
@@ -0,0 +1,85 @@
+
+
+DECLARE start_date, end_date DATE;
+SET start_date = "2022-01-01"; -- able to change start_date here
+SET end_date = "2022-02-01"; -- able to change end_date here
+
+  SELECT
+    o.global_entity_id ,
+    o.code as order_code,
+    o.payment_type.title as payment_type,
+    o.country_name AS country,
+    pd_vendors.location.city AS vendor_city,
+    --o.delivery_provider,
+    o.expedition_type,
+    o.vendor_code,
+    o.vendor_name,
+    o.is_billable,
+    o.is_own_delivery,
+    o.is_valid_order,
+    o.container_price_local,
+    o.total_value_local,
+    o.delivery_fee_local,
+    o.delivery_fee_original_local,
+    o.status_code,
+    o.status_name,
+    o.created_date_local,
+    o.ordered_at_local,
+    acc.initial_gfv_local,
+    acc.gfv_local,
+    acc.total_food_price_local,
+    acc.vat_amount_local,
+    CASE when vo.voucher.value_local is NULL then 0
+     ELSE vo.voucher.value_local
+      END AS total_voucher,
+    CASE when vo.voucher.foodpanda_subsidized_value_local is NULL then 0
+     ELSE round(vo.voucher.foodpanda_subsidized_value_local,2)
+      END as foodpanda_voucher,
+    CASE when vo.voucher.vendor_subsidized_value_local is NULL then 0
+     ELSE round(vo.voucher.vendor_subsidized_value_local,2)
+      END as vendor_voucher,
+    CASE when dis.discount.discount_amount_local is NULL then 0
+     ELSE round(dis.discount.discount_amount_local,2)
+      END as total_discount,
+    CASE when dis.discount.foodpanda_subsidized_value_local is NULL then 0
+     ELSE round(dis.discount.foodpanda_subsidized_value_local,2)
+      END as foodpanda_disc,
+    CASE when dis.discount.vendor_subsidized_value_local is null then 0
+     ELSE round(dis.discount.vendor_subsidized_value_local,2)
+      END as vendor_disc,
+-----------
+    CASE when vo.voucher.foodpanda_subsidized_value_local is NULL then 0
+     ELSE round(vo.voucher.foodpanda_subsidized_value_local,2) END
+       + CASE when dis.discount.foodpanda_subsidized_value_local is NULL then 0
+     ELSE round(dis.discount.foodpanda_subsidized_value_local,2)
+      END as total_fp_disc_voucher, --- total fp disc+voucher
+-----------
+    CASE when vo.voucher.vendor_subsidized_value_local is NULL then 0
+     ELSE round(vo.voucher.vendor_subsidized_value_local,2) END
+     + CASE when dis.discount.vendor_subsidized_value_local is null then 0
+     ELSE round(dis.discount.vendor_subsidized_value_local,2)
+     END as total_vendor_disc_voucher, --- total vendor disc+voucher
+    FROM `fulfillment-dwh-production.pandata_curated.pd_orders` o
+    LEFT JOIN `fulfillment-dwh-production.pandata_curated.pd_orders_agg_accounting` acc
+      ON o.uuid = acc.uuid
+      AND acc.created_date_utc BETWEEN DATE_SUB(start_date, INTERVAL 1 DAY) AND end_date
+    LEFT JOIN `fulfillment-dwh-production.pandata_curated.pd_orders_agg_vouchers` vo
+      ON o.uuid = vo.uuid
+      and vo.created_date_utc between DATE_SUB(start_date, INTERVAL 1 DAY) and end_date
+    left join `fulfillment-dwh-production.pandata_curated.pd_orders_agg_discounts` dis
+      on o.uuid = dis.uuid
+      and dis.created_date_utc between DATE_SUB(start_date, INTERVAL 1 DAY) and end_date
+    left join `fulfillment-dwh-production.pandata_curated.pd_vendors` pd_vendors
+      on pd_vendors.uuid = o.pd_vendor_uuid
+  WHERE TRUE
+    AND o.is_billable
+    AND NOT o.is_test_order
+    AND o.ordered_at_date_local between DATE_SUB(start_date, INTERVAL 1 DAY) and end_date
+    AND o.country_name = 'Cambodia'
+    AND o.global_entity_id = 'FP_KH'
+    AND o.created_date_utc between DATE_SUB(start_date, INTERVAL 1 DAY) and end_date
+
+
+
+
+
--- BQDataAnalytics/OwnResources/ShopDealPerformanceComparison.sql
+++ BQDataAnalytics/OwnResources/ShopDealPerformanceComparison.sql
@@ -1,5 +1,4 @@
 
-
 DECLARE date1, date2, date3 DATE;
 SET date1 = DATE_TRUNC(DATE_SUB(CURRENT_DATE(),INTERVAL 3 month),month);
 SET date2 = DATE_TRUNC(DATE_SUB(CURRENT_DATE(),INTERVAL 2 month),month);
@@ -21,13 +20,16 @@ WITH deal_table AS(
     di.amount_local, 
     di.foodpanda_ratio,
     IFNULL(expedition_types,"delivery") AS expedition_types,
-  
-
     DATE_DIFF(DATE(di.end_date_local), DATE(di.start_date_local), DAY) AS number_day_of_campaign,
+
     COUNT(DISTINCT CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local THEN o.code END) AS total_orders_during_campaign,
     COUNT(DISTINCT CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local 
       AND o.is_billable IS TRUE AND o.is_valid_order IS FALSE THEN o.code END) AS total_wastages_during_campaign,
-    ROUND(SUM(CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local THEN a.gmv_local END),2) AS total_gmv_local_during_campaign,
+    ROUND(SUM(CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local AND d.is_discount_used THEN a.gfv_local END),2) AS total_gfv_local_during_campaign,
+    SUM(CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local AND d.is_discount_used THEN a.gfv_local END)/COUNT(DISTINCT CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local THEN o.code END) as afv_local_during_campaign,
+    ROUND(SUM(CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local AND d.is_discount_used THEN discount.foodpanda_subsidized_value_local END),2) AS foodpanda_paid,
+    COUNT(DISTINCT CASE WHEN o.created_date_local BETWEEN di.start_date_local AND di.end_date_local AND d.is_discount_used AND is_first_valid_order_shops THEN o.pd_customer_id END) as total_shop_nc_during_campaign,
+
 
     FROM `fulfillment-dwh-production.pandata_curated.pd_orders` AS o
     INNER JOIN `fulfillment-dwh-production.pandata_curated.pd_orders_agg_discounts` AS d
@@ -43,6 +45,11 @@ WITH deal_table AS(
     LEFT JOIN `fulfillment-dwh-production.pandata_curated.pd_orders_agg_accounting` AS a
         ON o.global_entity_id = a.global_entity_id
         AND o.uuid = a.uuid
+    LEFT JOIN `fulfillment-dwh-production.pandata_report.marketing_pd_orders_agg_acquisition_dates` m
+        ON o.global_entity_id =m.global_entity_id
+        AND o.uuid=m.uuid
+        -- catch new customer with last 3 months
+        AND m.created_date_utc >=DATE_TRUNC(DATE_SUB(CURRENT_DATE(),INTERVAL 3 month),month)
   WHERE 
     v.global_entity_id = 'FP_KH'
     AND v.is_active
@@ -51,13 +58,16 @@ WITH deal_table AS(
     AND NOT v.is_test
     AND o.is_billable
     AND o.is_test_order IS FALSE
+
     AND o.created_date_utc >= date1
     AND a.created_date_utc >= date1
     AND d.created_date_utc >= date1
     
     --set start_date_local sub within last 3months
     AND di.start_date_local >= date1
+
   GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14
+
 ), none_deal_table AS(
   SELECT
     o.created_date_local,
@@ -76,8 +86,14 @@ WITH deal_table AS(
       END ) AS total_wastages,
     SUM(
       CASE 
-        WHEN o.is_valid_order IS TRUE THEN a.gmv_local 
-      END ) AS total_gmv_local,
+        WHEN o.is_valid_order IS TRUE THEN a.gfv_local 
+      END ) AS total_gfv_local,
+    COUNT(DISTINCT 
+      CASE WHEN m.is_first_valid_order_shops IS TRUE 
+      THEN o.pd_customer_id END) 
+    AS total_shop_nc,
+
+
     FROM `fulfillment-dwh-production.pandata_curated.pd_orders` AS o
     LEFT JOIN `fulfillment-dwh-production.pandata_curated.pd_vendors` AS v
         ON o.global_entity_id = v.global_entity_id
@@ -85,6 +101,13 @@ WITH deal_table AS(
     LEFT JOIN `fulfillment-dwh-production.pandata_curated.pd_orders_agg_accounting` AS a
       ON o.global_entity_id = a.global_entity_id
       AND o.uuid = a.uuid
+    LEFT JOIN `fulfillment-dwh-production.pandata_report.marketing_pd_orders_agg_acquisition_dates` m
+        ON o.global_entity_id =m.global_entity_id
+        AND o.uuid=m.uuid
+        AND m.created_date_utc >=DATE_TRUNC(DATE_SUB(CURRENT_DATE(),INTERVAL 5 month),month)
+    LEFT JOIN `fulfillment-dwh-production.pandata_curated.pd_vendors_agg_business_types` AS vb
+      ON v.vendor_code = vb.vendor_code
+      AND v.global_entity_id = vb.global_entity_id
   WHERE 
     v.global_entity_id = 'FP_KH'
     AND v.is_active
@@ -92,6 +115,12 @@ WITH deal_table AS(
     AND NOT v.is_test
     AND o.is_billable
     AND o.is_test_order IS FALSE
+
+    --AND v.vertical = 'shop'
+    --AND v.vertical_type = 'shop'
+    --AND vb.is_shops
+    --AND vb.business_type_apac IN ('shops')
+
     AND o.created_date_utc >= DATE_TRUNC(DATE_SUB(CURRENT_DATE(),INTERVAL 5 month),month)
     AND a.created_date_utc >= DATE_TRUNC(DATE_SUB(CURRENT_DATE(),INTERVAL 5 month),month)
   GROUP BY 1,2,3
@@ -112,16 +141,21 @@ WITH deal_table AS(
     d.end_date_local,
     d.amount_local, 
     d.foodpanda_ratio,
+    d.foodpanda_paid,
     d.number_day_of_campaign,
 
 
     d.total_orders_during_campaign,
     d.total_wastages_during_campaign,
-    ROUND(d.total_gmv_local_during_campaign,2) AS total_gmv_local_during_campaign,
+    d.total_shop_nc_during_campaign,
+    d.total_gfv_local_during_campaign AS total_gfv_local_during_campaign,
+    d.afv_local_during_campaign AS afv_local_during_campaign,
 
+    IFNULL(SUM(n.total_gfv_local),0) AS total_gfv_local_before_campaign,
+    IFNULL(SAFE_DIVIDE(SUM(n.total_gfv_local), SUM(n.total_orders)),0) AS afv_local_before_campaign,
     IFNULL(SUM(n.total_orders),0) AS total_orders_before_campaign,
     IFNULL(SUM(n.total_wastages),0) AS total_wastages_before_campaign,
-    IFNULL(ROUND(SUM(n.total_gmv_local),2),0) AS total_gmv_local_before_campaign,
+    IFNULL(SUM(n.total_shop_nc),0) AS total_shop_nc_before_campaign,
 
   FROM deal_table AS d
   LEFT JOIN none_deal_table AS n
@@ -129,7 +163,8 @@ WITH deal_table AS(
     AND d.expedition_types = n.expedition_types
     AND n.created_date_local <= d.start_date_local
     AND n.created_date_local >= d.start_date_local - DATE_DIFF(DATE(d.end_date_local), DATE(d.start_date_local), DAY)
-  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
+  
 ), result_table_v2 AS(
     SELECT 
       d.vendor_code,
@@ -145,27 +180,26 @@ WITH deal_table AS(
       d.end_date_local,
       d.amount_local, 
       d.foodpanda_ratio,
+      d.foodpanda_paid,
       d.number_day_of_campaign,
+      
+      STRING_AGG(d.expedition_types, ", " ORDER BY LENGTH(d.expedition_types) DESC ) AS expedition_types, 
 
-      STRING_AGG(d.expedition_types, "," ORDER BY LENGTH(d.expedition_types) DESC ) AS expedition_types,
       SUM(d.total_orders_during_campaign) AS total_orders_during_campaign,
       SUM(d.total_wastages_during_campaign) AS total_wastages_during_campaign,
-      ROUND(SUM(d.total_gmv_local_during_campaign),2) AS total_gmv_local_during_campaign,
+      ROUND(SUM(d.total_gfv_local_during_campaign),2) AS total_gfv_local_during_campaign,
+      ROUND(SUM(d.afv_local_during_campaign),2) AS afv_local_during_campaign,
+      SUM(d.total_shop_nc_during_campaign) AS total_shop_nc_during_campaign,
 
       SUM(d.total_orders_before_campaign) AS total_orders_before_campaign,
       SUM(d.total_wastages_before_campaign) AS total_wastages_before_campaign,
-      ROUND(SUM(d.total_gmv_local_before_campaign),2) AS total_gmv_local_before_campaign,
+      ROUND(SUM(d.total_gfv_local_before_campaign),2) AS total_gfv_local_before_campaign,
+      ROUND(SUM(d.afv_local_before_campaign),2) AS afv_local_before_campaign,
+      SUM(d.total_shop_nc_before_campaign) AS total_shop_nc_before_campaign
+
   FROM result_table_v1 AS d
-  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14
+  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
 )
-SELECT * FROM result_table_v2 
-
-
---WHERE vendor_code = 'tleq'
---WHERE total_gmv_local_before_campaign IS NULL OR total_orders_before_campaign IS NULL OR total_wastages_before_campaign IS NULL OR expedition_types is null
---WHERE vendor_code='nk8c'
---ORDER BY vendor_code, discount_uuid
---WHERE vendor_code ='np0m'
---WHERE total_gmv_local_before_campaign IS NULL OR total_orders_before_campaign IS NULL OR total_wastages_before_campaign IS NULL
+SELECT * FROM result_table_v2 WHERE vendor_type NOT IN ('street_food', 'restaurants')
 
 
